ca479042c6c2de6d51cd362bc60660342c13706d
Sandeep Jassal <sandeep@daemonsolutions.com>
2016-06-21 22:15:05 +0100

    added list command

--- countdown.php
+++ countdown.php
@@ -12,6 +12,7 @@
 	$token = $_POST['token'];
 	$author = $_POST['user_name'];
 	$channel = $_POST['channel_id'];
+	$reply = '';
 	$post = false;
 
 	//validate that requests are coming from the correct slack team
@@ -29,76 +30,117 @@
 		//extract command parameters and validate that the correct number of been passed
 		$params = explode(' -', $text);
 
-		if(count($params) >= 2 && count($params) <= 3){
-			$eventDate = substr($params[0], 6);
-			$eventDescription = substr($params[1], 6);
+		if( substr($params[0], 1, 4) == 'list' ){
+			$sql = "SELECT * from countdown where date >= CURDATE();";
+			$result = $link->query($sql);
 
-			if(count($params) == 3){
-				$imgURL = substr($params[2], 4);
+			if( $result->num_rows > 0 ){
+				while( $row = $result->fetch_assoc()){
+					$reply = $reply."\n".$row['id']."\t»\t".$row['event']."\t»\t".date("d/m/Y", strtotime($row['date']))."\t»\t".$row['url']."\t»\t".$row['channel']."\t»\t".$row['author'];
+				}
+
+				$result->free();
+
+				$ch = curl_init($webhook_url);	//setup curl 
+
+				//build json array
+				$payload = array(
+					'channel' => '@'.$author,
+					'username' => 'countdown-bot',
+					'attachments' => array(
+						array(
+							'title' => 'Countdowns',
+							'text' => $reply;
+						)
+					),
+				);
+
+				$payloadEncoded = json_encode($payload);	//encode array into valid json string
+				curl_setopt($ch, CURLOPT_POST, 1);	//specify that we want to send a POST request
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadEncoded);	// attached JSON string to POST field
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));	//set the content type to application/json
+
+				$result = curl_exec($ch);	//execute curl to post message to slack and save any response codes
+
 			} else {
-				$imgURL = "";
+				echo "No countdowns set";
 			}
 
-		} else{
-			$msg = "ERROR: usage /countdown -date dd/mm/yyyy -event description [-img img_url]";
-			die($msg);
-			echo($msg);
-		}
-		
-		//insert new countdown into db
-		$sql = "INSERT INTO countdown (event, date, url, channel, author) VALUES ('".$eventDescription."', STR_TO_DATE('".$eventDate."', '%d/%m/%Y'), '".$imgURL."', '".$channel."', '".$author."');";
-		$link->query($sql);
-		if( $link->affected_rows == 1){
-			$post = true;
-		}else{
-			$post = false;
-			$reply = " There was a problem creating your countdown, please try again later";
-		}
+		} elseif( substr($params[0], 1, 6) == 'delete' ){ 
 
-		//if there was a successful insertion of a new countdown then post countdown to slack in channel it was created in
-		if($post == true){
-			$datetimeEvent = date_create_from_format('d/m/Y', $eventDate);
-			$datetimeNow = date_create_from_format('d/m/Y', date("d/m/Y"));
-			$datediff = $datetimeNow->diff($datetimeEvent);
+		} else {	
 
-			if( $datediff->format('%a') > 1 || $datediff->format('%a') == 0){
-				$postText = $datediff->format('%a')." days to go until ".$eventDescription;
-			} else {
-				$postText = $datediff->format('%a')." day to go until ".$eventDescription;
+			if(count($params) >= 2 && count($params) <= 3){
+				$eventDate = substr($params[0], 6);
+				$eventDescription = substr($params[1], 6);
+
+				if(count($params) == 3){
+					$imgURL = substr($params[2], 4);
+				} else {
+					$imgURL = "";
+				}
+
+			} else{
+				$msg = "ERROR: usage /countdown -date dd/mm/yyyy -event description [-img img_url]";
+				die($msg);
+				echo($msg);
+			}
+			
+			//insert new countdown into db
+			$sql = "INSERT INTO countdown (event, date, url, channel, author) VALUES ('".$eventDescription."', STR_TO_DATE('".$eventDate."', '%d/%m/%Y'), '".$imgURL."', '".$channel."', '".$author."');";
+			$link->query($sql);
+			if( $link->affected_rows == 1){
+				$post = true;
+			}else{
+				$post = false;
+				$reply = " There was a problem creating your countdown, please try again later";
 			}
 
-			$footer = 'countdown created by '.$author;
+			//if there was a successful insertion of a new countdown then post countdown to slack in channel it was created in
+			if($post == true){
+				$datetimeEvent = date_create_from_format('d/m/Y', $eventDate);
+				$datetimeNow = date_create_from_format('d/m/Y', date("d/m/Y"));
+				$datediff = $datetimeNow->diff($datetimeEvent);
 
-			$ch = curl_init($webhook_url);	//setup curl
+				if( $datediff->format('%a') > 1 || $datediff->format('%a') == 0){
+					$postText = $datediff->format('%a')." days to go until ".$eventDescription;
+				} else {
+					$postText = $datediff->format('%a')." day to go until ".$eventDescription;
+				}
 
-			//build json array
-			$payload = array(
-				'channel' => $channel,
-				'username' => 'countdown-bot',
-				'attachments' => array(
-					array(
-						'title' => $postText,
-						'image_url' => $imgURL,
-						'footer' => $footer
-					)
-				),
-			);
+				$footer = 'countdown created by '.$author;
 
-			$payloadEncoded = json_encode($payload);	//encode array into valid json string
-			curl_setopt($ch, CURLOPT_POST, 1);			//specify that it is a POST request
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadEncoded);	//attach json string to POST field
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));	//set content type to application/json
+				$ch = curl_init($webhook_url);	//setup curl
 
-			$result = curl_exec($ch);	//execute curl and save any response codes
+				//build json array
+				$payload = array(
+					'channel' => $channel,
+					'username' => 'countdown-bot',
+					'attachments' => array(
+						array(
+							'title' => $postText,
+							'image_url' => $imgURL,
+							'footer' => $footer
+						)
+					),
+				);
+
+				$payloadEncoded = json_encode($payload);	//encode array into valid json string
+				curl_setopt($ch, CURLOPT_POST, 1);			//specify that it is a POST request
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $payloadEncoded);	//attach json string to POST field
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));	//set content type to application/json
+
+				$result = curl_exec($ch);	//execute curl and save any response codes
+			}
+
+			//clear variables
+			unset($postText);
+			unset($payload);
+			unset($ch);
+			
+			//respond to slack with success or failure of checkin, this iwll be visible to the user only
+			echo $reply;
 		}
-
-		//clear variables
-		unset($postText);
-		unset($payload);
-		unset($ch);
-		
-		//respond to slack with success or failure of checkin, this iwll be visible to the user only
-		echo $reply;
 	}
 
 	//close connection to the database
