8be4ee37d77337bfc951b48b9adccb6750dfcbad
Sandeep Jassal <sandeep@daemonsolutions.com>
2016-06-21 22:20:21 +0100

    tweaks

--- countdown.php
+++ countdown.php
@@ -36,7 +36,7 @@
 
 			if( $result->num_rows > 0 ){
 				while( $row = $result->fetch_assoc()){
-					$reply = $reply."\n".$row['id']." » ".$row['event']." » ".date("d/m/Y", strtotime($row['date']))." » ".$row['url']." » ".$row['channel']." » ".$row['author'];
+					$reply = $reply."\n".$row['id']." » ".$row['event']." » ".date("d/m/Y", strtotime($row['date']))." » ".$row['url']." » ".$row['channel']." » ".$row['author']."\n";
 				}
 
 				$result->free();
