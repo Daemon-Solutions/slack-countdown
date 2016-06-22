5101f1b817330963956946a1f667a4c2f92a94fa
root <root@ip-172-31-24-171.eu-west-1.compute.internal>
2016-06-22 12:05:36 +0000

    added mysql_schema

---
+++ mysql_schema.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.6.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: countdown
+-- ------------------------------------------------------
+-- Server version	5.6.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `countdown`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `countdown`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `countdown` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `countdown`;
+
+--
+-- Table structure for table `countdown`
+--
+
+DROP TABLE IF EXISTS `countdown`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `countdown` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event` varchar(400) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `url` varchar(400) DEFAULT NULL,
+  `channel` varchar(20) DEFAULT NULL,
+  `author` varchar(140) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-22 12:04:22
